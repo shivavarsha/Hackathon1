1624718dd125c17dd233d840b5f8f638fc71b37a
shivavarsha <88608996+shivavarsha@users.noreply.github.com>
2021-11-21 17:41:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+async function getBreweries() {
+    try {
+      const dataList = await fetch("https://api.openbrewerydb.org/breweries", {
+        method: "GET"
+      });
+      let data = await dataList.json();
+   const breweriesList = document.createElement("div");
+    console.log("hi");
+    document.body.append(breweriesList);
+
+    console.log(data);
+    data.forEach(element => {
+        let val;
+        val="Name : ";
+        val=val+element.name;
+        document.body.append(val);
+       let linebreak=document.createElement("br");
+        document.body.append(linebreak);
+       val="Type : ";
+        val=val+element.brewery_type;
+         document.body.append(val);
+         linebreak=document.createElement("br");
+        document.body.append(linebreak);       
+        val="Street : ";
+        val=val+element.street;
+        document.body.append(val);
+         linebreak=document.createElement("br");
+        document.body.append(linebreak);
+        val="URL : ";
+        val=val+element.website_url;
+        document.body.append(val);
+        linebreak=document.createElement("br");
+        document.body.append(linebreak);
+        val="Phone No:";
+        val=val+element.phone;
+        document.body.append(val);
+       linebreak=document.createElement("br");
+        document.body.append(linebreak);
+        linebreak=document.createElement("br");
+        document.body.append(linebreak);
+        
+      });
+
+} catch (error) {
+    console.log(error.message);
+  }
+}
